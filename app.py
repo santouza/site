5f80c3f0229365c35ffe0bb7646a97455f5c3486
Lívia <santouza5@gmail.com>
2019-11-21 16:25:17 -0300

    Não mudei nada

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
-from forms import CadastroUsuario
+from forms import CadastroUsuario, Doacoes
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///CSD.db'
@@ -17,6 +17,16 @@ class User(db.Model):
     def __repr__(self):
         return '<User %r>' % self.username
 
+class Doar(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    n_pecas = db.Column(db.Integer)
+    d_pecas = db.Column(db.String(150), nullable = False)
+
+    def __repr__(self):
+        return '<Doar %r>' % self.n_pecas
+
+db.create_all()
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -24,14 +34,20 @@ def index():
 @app.route('/cadastrar', methods = ['GET', 'POST'])
 def cadastrar():
     form = CadastroUsuario()
+    if form.validate_on_submit ():
+        user = User()
+        user.username = form.username.data
+        user.email = form.email.data
+        user.password = form.password.data
+        db.session.add(user)
+        db.session.commit()
 
     return render_template('register.html', form = form)
 
-@app.route('/', methods = ['GET', 'POST'])
-def login():
-    form = LoginUsuario()
-
-    return render_template('index.html', form = form)
+@app.route('/doar')
+def doacoes():
+    form = Doacoes()
+    return render_template('doacoes.html', form = form)
 
 if __name__ == ('__main__'):
     app.run(debug=True)
