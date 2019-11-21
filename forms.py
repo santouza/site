89b6a58b1efb5f0e56d2ba5e0b188f9a8fa5ece4
Lívia <santouza5@gmail.com>
2019-11-21 16:19:09 -0300

    Inserir novo formulario Doacoes

--- forms.py
+++ forms.py
@@ -1,10 +1,16 @@
+#coding: utf-8
 from flask_wtf import FlaskForm
-from wtforms import (StringField, PasswordField, SubmitField, BooleanField)
+from wtforms import (StringField, PasswordField, SubmitField, BooleanField, TextAreaField)
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
-class CadatroUsuario(FlaskForm):
-    username = StringField('Usuário', validators = [DataRequired(), Length(min = 2, mas = 80)])
+class CadastroUsuario(FlaskForm):
+    username = StringField('Usuário', validators = [DataRequired(), Length(min = 2, max = 80)])
     email = StringField('Email', validators = [DataRequired(), Email()])
     password = PasswordField('Senha', validators = [DataRequired()])
     conf_password = PasswordField('Confirme sua senha', validators = [DataRequired(), EqualTo('password')])
-    submit = SumbmitField('Cadastrar')
+    submit = SubmitField('Cadastrar')
+
+class Doacoes(FlaskForm):
+    n_pecas = StringField('Nº de peças', validators = [DataRequired()])
+    d_pecas = TextAreaField('Descrição das peças', validators = [DataRequired()])
+    botao = SubmitField('Doar')
